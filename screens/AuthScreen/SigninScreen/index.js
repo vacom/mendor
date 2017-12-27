294691193e17fb45f4f6ddcaa842de1a83f40417
Vitor Amaral <va.works@hotmail.com>
2017-12-27 19:24:45 +0000

    Auth Flow
    Auth:
    - All the roots are now protect need to signin to continue;
    - is now possible to login with a user
    - is now possible to create a basic account;

--- screens/AuthScreen/SigninScreen/index.js
+++ screens/AuthScreen/SigninScreen/index.js
@@ -1,66 +1,122 @@
 import React from "react";
-import {Text, Container, Content, Form, Item, Input, Label, Fab} from "native-base";
-import {Row} from "react-native-easy-grid";
-import styled from "styled-components/native";
-import {LinearGradient} from "expo";
-import {MaterialIcons} from "@expo/vector-icons";
-
+import { NavigationActions } from "react-navigation";
 //Components
-
-class AuthScreen extends React.Component {
-    static navigationOptions = {
-        title: "",
-    };
-
-    render() {
-        return (
-            <ScreenContainer>
-                <LinearGradient colors={["#3f51b5", "#B39DDB"]}>
-                    <ContentContainer>
-                        <Row style={{height: 'auto', marginBottom: 15, marginTop: 30, backgroundColor: 'transparent'}}>
-                            <Text style={{fontSize: 26, fontWeight: '600', color: "#fff"}}>
-                                Entrar
-                            </Text>
-                        </Row>
-                        <Row style={{height: 'auto', backgroundColor: 'transparent', marginBottom: 30}}>
-                            <Text style={{
-                                fontSize: 16,
-                                lineHeight: 24,
-                                color: '#fff',
-                            }}>
-                                Bem vindo de volta, no mendor és sempre recebido com muito amor.
-                            </Text>
-                        </Row>
-                    </ContentContainer>
-                </LinearGradient>
-                <Container>
-                    <Content style={{paddingLeft: 20, paddingRight: 20}}>
-                        <Form style={{paddingBottom: 60}}>
-                            <Item style={{marginLeft: 0}} floatingLabel>
-                                <Label style={{color: '#757575'}}>Email</Label>
-                                <Input />
-                            </Item>
-                            <Item style={{marginLeft: 0}} floatingLabel>
-                                <Label style={{color: '#757575'}}>Password</Label>
-                                <Input />
-                            </Item>
-                        </Form>
-                    </Content>
-                </Container>
-                <Fab
-                    direction="up"
-                    containerStyle={{ }}
-                    style={{backgroundColor: '#3f51b5'}}
-                    position="bottomRight"
-                >
-                    <MaterialIcons name="arrow-forward" size={24} color="#ffffff"/>
-                </Fab>
-            </ScreenContainer>
-        );
+import {
+  Text,
+  Container,
+  Content,
+  Form,
+  Item,
+  Input,
+  Label,
+  Fab
+} from "native-base";
+import { Row } from "react-native-easy-grid";
+import styled from "styled-components/native";
+import { LinearGradient } from "expo";
+import { MaterialIcons } from "@expo/vector-icons";
+//GraphQL
+import { graphql, compose } from "react-apollo";
+import { SIGNIN_USER_MUTATION } from "../../../api/Mutations/User";
+import { USER_SIGNIN_FUNC } from "../../../api/Functions/User";
+//Utils
+import Toast from "react-native-root-toast";
+class SigninScreen extends React.Component {
+  static navigationOptions = {
+    title: ""
+  };
+  state = {
+    email: "",
+    password: ""
+  };
+  _onUserSignIn = async () => {
+    const { email, password } = this.state;
+    const { signinUser, navigation } = this.props;
+    //Signins the user and checks in the DB
+    const result = await USER_SIGNIN_FUNC(email, password, signinUser);
+    //If it passes goes to the main screen
+    if (result.status) {
+      const resetAction = NavigationActions.reset({
+        index: 0,
+        actions: [NavigationActions.navigate({ routeName: "Main" })]
+      });
+      navigation.dispatch(resetAction);
+    } else {
+      console.log("error = ", result.error);
+      Toast.show("Erro! Verifique os campos.");
     }
+  };
+  render() {
+    return (
+      <ScreenContainer>
+        <LinearGradient colors={["#3f51b5", "#B39DDB"]}>
+          <ContentContainer>
+            <Row
+              style={{
+                height: "auto",
+                marginBottom: 15,
+                marginTop: 30,
+                backgroundColor: "transparent"
+              }}
+            >
+              <Text style={{ fontSize: 26, fontWeight: "600", color: "#fff" }}>
+                Entrar
+              </Text>
+            </Row>
+            <Row
+              style={{
+                height: "auto",
+                backgroundColor: "transparent",
+                marginBottom: 30
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 16,
+                  lineHeight: 24,
+                  color: "#fff"
+                }}
+              >
+                Bem vindo de volta, no mendor és sempre recebido com muito amor.
+              </Text>
+            </Row>
+          </ContentContainer>
+        </LinearGradient>
+        <Container>
+          <Content style={{ paddingLeft: 20, paddingRight: 20 }}>
+            <Form style={{ paddingBottom: 60 }}>
+              <Item style={{ marginLeft: 0 }} floatingLabel>
+                <Label style={{ color: "#757575" }}>Email</Label>
+                <Input onChangeText={email => this.setState({ email })} />
+              </Item>
+              <Item style={{ marginLeft: 0 }} floatingLabel>
+                <Label style={{ color: "#757575" }}>Password</Label>
+                <Input
+                  secureTextEntry={true}
+                  onChangeText={password => this.setState({ password })}
+                />
+              </Item>
+            </Form>
+          </Content>
+        </Container>
+
+        <Fab
+          onPress={this._onUserSignIn}
+          direction="up"
+          containerStyle={{}}
+          style={{ backgroundColor: "#3f51b5" }}
+          position="bottomRight"
+        >
+          <MaterialIcons name="arrow-forward" size={24} color="#ffffff" />
+        </Fab>
+      </ScreenContainer>
+    );
+  }
 }
 
-export default AuthScreen;
+export default compose(graphql(SIGNIN_USER_MUTATION, { name: "signinUser" }))(
+  SigninScreen
+);
 
 const ScreenContainer = styled.View`
   flex: 1;
