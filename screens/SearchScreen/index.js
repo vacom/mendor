a52d3b4e6990e9aace4efb916ab4dac76882aa06
Hugo Silva <hugomps11@gmail.com>
2018-01-02 23:13:28 +0000

    Search Screen

--- screens/SearchScreen/index.js
+++ screens/SearchScreen/index.js
@@ -1,14 +1,64 @@
 import React from "react";
-import { View, Text } from "native-base";
+import styled from "styled-components/native";
+import GradientContainer from "../../components/GradientContainer";
+import { ScrollView } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Header, Item, Input, Icon, Button, Text, View, Thumbnail } from 'native-base';
+import {
+    Card,
+    CardContainer,
+    CardLeft,
+    CardBody,
+    CardRight
+  } from "../../components/Card";
 
 class SearchScreen extends React.Component {
+    static navigationOptions = {
+        header: (
+            <Header searchBar>
+            <Item>
+              <Input placeholder="José Saramago" />
+            </Item>
+          </Header>
+        )
+      };
+
     render() {
         return (
-        <View>
-            <Text>Search</Text>
-        </View>
+  
+            <Container>
+                <GradientContainer>
+        <ScrollView>
+            <Card>
+              <CardContainer>
+                <CardLeft>
+                  <Thumbnail
+                    style={{ width: 48, height: 48 }}
+                    source={{
+                      uri:
+                        "https://images.gr-assets.com/authors/1497455560p5/1285555.jpg"
+                    }}
+                  />
+                </CardLeft>
+                <CardBody>
+                  <Text style={{ fontSize: 14, fontWeight: "bold", color: "#000" }}>José Saramago</Text>
+                  <Text style={{ fontSize: 14, color: "#757575" }}>Escritor</Text>
+                </CardBody>
+                <CardRight>
+                  <MaterialIcons name="more-vert" size={24} color="#757575" />
+                </CardRight>
+              </CardContainer>
+            </Card>
+        </ScrollView>
+        </GradientContainer>
+        </Container>
         )
     }
 }
 
 export default SearchScreen
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+`;
