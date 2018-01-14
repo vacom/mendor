cd37dd78f1d6b799dbcc055866a52ed6b50d60c8
Vasco <vascosilvaa@gmail.com>
2018-01-14 17:12:36 +0000

    add discussion new version

--- screens/DiscussionsScreen/AddScreen/index.js
+++ screens/DiscussionsScreen/AddScreen/index.js
@@ -2,18 +2,28 @@ import React from "react";
 import {
   Image,
   View,
-  Text,
   TouchableOpacity,
   Alert,
   KeyboardAvoidingView
 } from "react-native";
+import { LinearGradient } from "expo";
+import { Row } from "react-native-easy-grid";
 import { ImagePicker } from "expo";
 import styled from "styled-components/native";
-import { Form, Item, Input, Label, Button, Picker } from "native-base";
 import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 import Toast from "react-native-root-toast";
 import { graphql, compose, withApollo } from "react-apollo";
-
+import {
+  Text,
+  Container,
+  Content,
+  Form,
+  Item,
+  Input,
+  Label,
+  Button,
+  Picker
+} from "native-base";
 //REQUESTS
 import { CREATE_DISCUSSION_MUTATION } from "../../../api/Mutations/Discussions";
 import { ALL_CATEGORIES_QUERY } from "../../../api/Queries/Discussions";
@@ -33,11 +43,8 @@ class AddDiscussion extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      default_image:
-        "http://4globetrotters.world/wp-content/uploads/2015/08/Light-Grey-Background.jpg",
       categoryId: "0",
-      cover:
-        "http://4globetrotters.world/wp-content/uploads/2015/08/Light-Grey-Background.jpg",
+      cover: null,
       description: "",
       title: "",
       authorId: "cjbjhh0f9lbfz01142sd6tvuv"
@@ -76,12 +83,12 @@ class AddDiscussion extends React.Component {
   }
 
   submitDiscussion = () => {
-    const { title, description, categoryId, cover, default_image } = this.state;
+    const { title, description, categoryId, cover } = this.state;
     if (!title || !description || categoryId == 0) {
       Toast.show("Todos os campos são obrigatórios!");
       return;
     } else {
-      if (cover == default_image) {
+      if (!cover) {
         Alert.alert(
           "Imagem de fundo não definida",
           "Tem a certeza que deseja criar uma nova discussão sem imagem de fundo definida?",
@@ -154,79 +161,124 @@ class AddDiscussion extends React.Component {
       let { cover } = this.state;
       const resizeMode = "cover";
       return (
-        <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
-          <View style={{ flex: 0.3 }}>
-            <Background>
-              <Image
-                style={{
-                  flex: 1,
-                  resizeMode
-                }}
-                source={{ uri: this.state.cover }}
-              />
-            </Background>
-            <Content>
-              <StyledText>Carregar imagem de fundo</StyledText>
-              <Button
-                onPress={this._pickImage}
-                style={{
-                  backgroundColor: "#3F51B5",
-                  borderRadius: 2,
-                  alignSelf: "center",
-                  padding: 15
-                }}
-              >
-                <Text
-                  style={{
-                    color: "#fff",
-                    fontSize: 14,
-                    fontWeight: "600"
-                  }}
-                >
-                  {"carregar".toUpperCase()}
-                </Text>
-              </Button>
-            </Content>
-          </View>
-          <View style={{ flex: 0.7, marginTop: 10 }}>
-            <Form>
-              <Item>
-                <Input
-                  placeholder="Título"
-                  onChangeText={title => this.setState({ title })}
-                />
-              </Item>
-              <Item>
-                <Input
-                  multiline={true}
-                  numberOfLines={8}
-                  placeholder="Descrição"
-                  onChangeText={description => this.setState({ description })}
-                />
-              </Item>
-              <StyledPickerView>
-                <Picker
-                  style={{ color: "#6B6A6F" }}
-                  mode="dropdown"
-                  onValueChange={this.onPickerChange.bind(this)}
-                  selectedValue={this.state.categoryId}
-                >
-                  <Item style={{ fontSize: 20 }} label="Categoria" value="0" />
-                  {this.props.Categories.allCategories.map(data => {
-                    return (
+        <ScreenContainer>
+          <LinearGradient colors={["#3f51b5", "#B39DDB"]}>
+            <ContentContainer>
+              <DashedContainer>
+                {cover != null && (
+                  <Background>
+                    <Image
+                      style={{
+                        flex: 1,
+                        borderRadius: 3,
+                        resizeMode
+                      }}
+                      source={{ uri: cover }}
+                    />
+                  </Background>
+                )}
+                <View style={{ padding: 30, alignItems: 'center' }}>
+                  <Row
+                    style={{ height: "auto", backgroundColor: "transparent" }}
+                  >
+                    <MaterialIcons
+                      name="file-upload"
+                      size={42}
+                      color="#ffffff"
+                    />
+                  </Row>
+                  <Row
+                    style={{
+                      height: "auto",
+                      marginBottom: 15,
+                      marginTop: 6,
+                      backgroundColor: "transparent"
+                    }}
+                  >
+                    <Text
+                      style={{
+                        fontSize: 16,
+                        fontWeight: "400",
+                        color: "#fff"
+                      }}
+                    >
+                      Carregar imagem de fundo
+                    </Text>
+                  </Row>
+                  <Row
+                    style={{ height: "auto", backgroundColor: "transparent" }}
+                  >
+                    <Button
+                      style={{ backgroundColor: "#3F51B5", borderRadius: 2 }}
+                      onPress={this._pickImage}
+                    >
+                      <Text
+                        style={{
+                          fontSize: 14,
+                          fontWeight: "600"
+                        }}
+                      >
+                        {"carregar".toUpperCase()}
+                      </Text>
+                    </Button>
+                  </Row>
+                </View>
+              </DashedContainer>
+            </ContentContainer>
+          </LinearGradient>
+          <Container>
+            {this.state.loading ? (
+              <Loading dark />
+            ) : (
+              <Content style={{ paddingLeft: 20, paddingRight: 20 }}>
+                <Form style={{ paddingBottom: 60 }}>
+                  <Item style={{ marginLeft: 0 }} floatingLabel>
+                    <Label style={{ color: "#757575" }}>Título</Label>
+                    <Input
+                      onChangeText={title => this.setState({ title })}
+                      value={this.state.title}
+                    />
+                  </Item>
+                  <Item style={{ marginLeft: 0 }} floatingLabel>
+                    <Label style={{ color: "#757575" }}>Descrição</Label>
+                    <Input
+                      multiline={true}
+                      numberOfLines={6}
+                      value={this.state.description}
+                      onChangeText={description =>
+                        this.setState({ description })
+                      }
+                    />
+                  </Item>
+                  <StyledPickerView>
+                    <Picker
+                      style={{ color: "#6B6A6F" }}
+                      mode="dropdown"
+                      onValueChange={this.onPickerChange.bind(this)}
+                      selectedValue={this.state.categoryId}
+                    >
                       <Item
-                        style={{ fontSize: 20 }}
-                        label={data.title}
-                        value={data.id}
-                        key={data.id}
+                        style={{ fontSize: 20, margin: 0 }}
+                        label="Categoria"
+                        value="0"
                       />
-                    );
-                  })}
-                </Picker>
-              </StyledPickerView>
-            </Form>
-          </View>
-        </KeyboardAvoidingView>
+                      {this.props.Categories.allCategories.map(data => {
+                        return (
+                          <Item
+                            style={{ fontSize: 20 }}
+                            label={data.title}
+                            value={data.id}
+                            key={data.id}
+                          />
+                        );
+                      })}
+                    </Picker>
+                  </StyledPickerView>
+                </Form>
+              </Content>
+            )}
+          </Container>
+        </ScreenContainer>
       );
     }
   }
@@ -250,29 +302,33 @@ const AddDiscussionWithData = compose(
 
 export default withApollo(AddDiscussionWithData);
 
+const ScreenContainer = styled.View`
+  flex: 1;
+  background-color: #fff;
+`;
+
+const ContentContainer = styled.View`
+  margin: 30px;
+`;
+
+const DashedContainer = styled.View`
+  padding: 0;
+  border: 1px dashed #ffffff;
+  align-items: center;
+  border-radius: 6px;
+`;
+const StyledPickerView = styled.View`
+  padding: 0;
+  margin: 30px 0;
+  border-style: solid;
+  border-bottom-width: 1px;
+  border-bottom-color: #dedde3;
+`;
+
 const Background = styled.View`
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-`;
-
-const Content = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-`;
-
-const StyledText = styled.Text`
-  font-size: 18px;
-  margin-bottom: 10px;
-`;
-
-const StyledPickerView = styled.View`
-  padding: 10px 5px 0px 0px;
-  margin: 30px 0px 0px 15px;
-  border-style: solid;
-  border-bottom-width: 1px;
-  border-bottom-color: #dedde3;
 `;
